d6d78c5d83f09cc1f8cab861ce634c34c82e10bb
Keith Rieck <rieck_55113@yahoo.com>
2017-11-25 13:42:05 -0600

    update examples

--- examples/Animation_pulse/PulseAnimation.h
+++ examples/Animation_pulse/PulseAnimation.h
@@ -7,6 +7,8 @@
  */
 class PulseAnimation : public Animation {
   public:
+    PulseAnimation();
+    PulseAnimation(int n);
     void reset(Adafruit_NeoPixel *strip);
     void draw(Adafruit_NeoPixel *strip);
     void setColor(int i, uint32_t c);
@@ -19,5 +21,7 @@ class PulseAnimation : public Animation {
   private:
     int f(unsigned long t);
     uint32_t fadeColor(uint32_t c, int brightness);
+    uint32_t *colorList;
+    int colorCount;
 };
 #endif
