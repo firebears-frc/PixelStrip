67a17cdd74f4f066e8bf613c689707c2fed6c463
Keith Rieck <rieck_55113@yahoo.com>
2017-11-25 12:21:04 -0600

    updated examples

--- examples/Animation_spinning/SpinningAnimation.cpp
+++ examples/Animation_spinning/SpinningAnimation.cpp
@@ -1,23 +1,23 @@
 #include <Adafruit_NeoPixel.h>
 #include "SpinningAnimation.h"
 
-uint32_t _black;
-int _current_pixel;
+uint32_t black;
+int current_pixel;
 
 void SpinningAnimation::reset(Adafruit_NeoPixel *strip) {
-  _black = strip->Color(0, 0, 0);
-  _current_pixel = 0;
-  _time_delay = 1000.0 / strip->numPixels();
+  black = strip->Color(0, 0, 0);
+  current_pixel = 0;
+  time_delay = 1000.0 / strip->numPixels();
 }
 
 void SpinningAnimation::draw(Adafruit_NeoPixel *strip) {
   if (isTimedout()) {
-    strip->setPixelColor(_current_pixel, _black);
-    _current_pixel = _current_pixel + 1;
-    if (_current_pixel >= strip->numPixels()) {
-      _current_pixel = 0;
+    strip->setPixelColor(current_pixel, black);
+    current_pixel = current_pixel + 1;
+    if (current_pixel >= strip->numPixels()) {
+      current_pixel = 0;
     }
-    strip->setPixelColor(_current_pixel, _color);
-    setTimeout(_time_delay);
+    strip->setPixelColor(current_pixel, color);
+    setTimeout(time_delay);
   }
 }
