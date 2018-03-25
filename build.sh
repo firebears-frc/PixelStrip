985c7015c18b5435e7d8d43670bfa114295863ac
keith <keith@10.0.0.11>
2018-03-25 12:23:07 -0500

    Added library.properties to match the Arduino 1.5 library specification. https://github.com/arduino/Arduino/wiki/Arduino-IDE-1.5:-Library-specification

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-VERSION=1.0.0
-zip -r PixelStrip.zip *.cpp *.h keywords.txt examples
+
+if [ $1 == "clean" ] ; then
+	rm -f PixelStrip.zip
+	rm -rf build
+else
+    mkdir -p build
+    cp src/*.cpp build
+    cp src/*.h build
+    cp -r examples build
+    cp keywords.txt build
+    cp README.md build
+    cp library.properties build
+    cd build
+	zip -r ../PixelStrip.zip *
+	cd ..
+fi
+
+
