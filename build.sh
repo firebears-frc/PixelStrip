d6116839a0a62fd6d38881dcebc2310b0210a1dc
Keith Rieck <KeithRieck@gmail.com>
2021-12-29 12:34:29 -0600

    Updated build.sh

--- build.sh
+++ build.sh
@@ -4,16 +4,15 @@ if [ $1 == "clean" ] ; then
 	rm -f PixelStrip.zip
 	rm -rf build
 else
-    mkdir -p build
-    cp src/*.cpp build
-    cp src/*.h build
-    cp -r examples build
-    cp keywords.txt build
-    cp README.md build
-    cp library.properties build
+    mkdir -p build/PixelStrip
+    cp src/*.cpp build/PixelStrip
+    cp src/*.h build/PixelStrip
+    cp -r examples build/PixelStrip
+    cp keywords.txt build/PixelStrip
+    cp README.md build/PixelStrip
+    cp library.properties build/PixelStrip
     cd build
-	zip -r ../PixelStrip.zip *
-	cd ..
+    zip -r ../PixelStrip.zip PixelStrip
+    cd ..
 fi
-
 
